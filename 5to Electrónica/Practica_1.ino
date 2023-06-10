11cd4b2038cd8789785aaf22017875954cf62cca
OmarEsteca2023 <136141538+OmarEsteca2023@users.noreply.github.com>
2023-06-09 21:19:07 -0600

    Update Practica_1.ino

--- 5to Electrónica/Practica_1.ino
+++ 5to Electrónica/Practica_1.ino
@@ -1,11 +1,13 @@
+const int LED = 1; //Definimos una constante tipo entero donde guardamos el valor del pin que usaremos   
+
 void setup() {//Modulo de configuración 
-pinMode(5,OUTPUT); //Definimos el Pin 1 del arduino como salida
+pinMode(LED,OUTPUT); //Definimos el Pin 1 del arduino como salida
 }
 
 void loop() {//Modulo de funcionamiento 
   
-digitalWrite (5,HIGH);//Encendemos el pin previamente configurado 
+digitalWrite (LED,HIGH);//Encendemos el pin previamente configurado 
 delay(1000);          //Mantenemos encendido el LED por 1 segundo
-digitalWrite (5,LOW); //Apagamos el pin previamente configurado 
+digitalWrite (LED,LOW); //Apagamos el pin previamente configurado 
 delay(1000);          //Mantenemos apagado el LED por 1 segundo
 }
